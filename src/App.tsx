a11b0169743355d1a5bd9671bf2d0af5cfb9a48a
Isa Reinert <reinert.isa@gmail.com>
2025-05-08 12:19:30 -0700

    feat(website): add more types to GreetProps

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Greet from "./components/Greet.tsx";
 function App() {
   return (
       <div>
-          <Greet name="Isa Reinert" />
+          <Greet name="Isa Reinert" messageCount={10} isLoggedIn={true} />
       </div>
   )
 }
