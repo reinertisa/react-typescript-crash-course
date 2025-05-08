b7c85f1fcef81b976b4d80d8857ca8b333a949ad
Isa Reinert <reinert.isa@gmail.com>
2025-05-08 12:22:34 -0700

    feat(website): typing object as prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
 import Greet from "./components/Greet.tsx";
+import Person from "./components/Person.tsx";
 
 function App() {
+    const name = {
+        first: 'Isa',
+        last: 'Reinert'
+    };
+
   return (
       <div>
           <Greet name="Isa Reinert" messageCount={10} isLoggedIn={true} />
+          <Person name={name} />
       </div>
   )
 }
---
+++ src/components/Person.tsx
@@ -0,0 +1,12 @@
+type PersonProps = {
+    name: {
+        first: string
+        last: string
+    }
+}
+
+export default function Person(props: PersonProps) {
+    return (
+        <div>{props.name.first} {props.name.last}</div>
+    )
+}
