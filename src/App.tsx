35bb57a9bb149fef5610ec7854a03862a2029db5
Isa Reinert <reinert.isa@gmail.com>
2025-05-08 17:39:21 -0700

    feat(website): add type with union

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Greet from "./components/Greet.tsx";
 import Person from "./components/Person.tsx";
 import PersonList from "./components/PersonList.tsx";
+import Status from "./components/Status.tsx";
 
 function App() {
     const name = {
@@ -29,6 +30,7 @@ function App() {
           <Greet name="Isa Reinert" messageCount={10} isLoggedIn={true} />
           <Person name={name} />
           <PersonList names={personList} />
+          <Status status='error' />
       </div>
   )
 }
