38a502423bec53859e5e4d3f334ce23f15cc89a1
Isa Reinert <reinert.isa@gmail.com>
2025-05-28 22:57:58 -0700

    feat(website): useRef with DOM

---
+++ src/components/useref/DomRef.tsx
@@ -0,0 +1,17 @@
+import {useRef, useEffect} from 'react';
+
+export default function DomRef() {
+
+    const inputRef = useRef<HTMLInputElement>(null!);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, [])
+
+    return (
+        <div>
+            <input ref={inputRef} type="text" />
+        </div>
+    )
+
+}
